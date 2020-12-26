0c594387a8d6ab21ffc626c628ad08eb898d3f57
jicking <jicking.dev@gmail.com>
2020-12-26 15:26:21 +0800

    Set inversion of control

---
+++ src/CleanApp.IOC/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using CleanApp.Application.Abstractions;
+using CleanApp.Application.Services;
+using CleanApp.Data.EF.Repositories;
+using CleanApp.Domain.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanApp.IOC
+{
+    class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //.Application
+            services.AddScoped<ITodoService, TodoServices>();
+
+            //.Domain | .Data
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
